d579f9b01c4d13db9514b0092d1abd6ddaf934e7
Aleksey Yuriev <alex11-90@yandex.by>
2025-06-14 19:15:08 +0200

    feature: update template for Home and Camera pages

--- src/pages/CameraPage.vue
+++ src/pages/CameraPage.vue
@@ -1,9 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round color="grey-10" icon="eva-camera" size="lg" />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="postData.caption"
+          label="Caption"
+          class="col col-sm-6"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="postData.location"
+          label="Location"
+          class="col col-sm-6"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { reactive } from 'vue';
+import { uid } from 'quasar';
+
+const postData = reactive({
+  id: uid(),
+  caption: '',
+  location: '',
+  photo: null,
+  date: Date.now(),
+});
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
