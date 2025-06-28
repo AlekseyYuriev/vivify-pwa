943d5cef5fc443bb08a4410d4cf12be02a081c8a
Aleksey Yuriev <alex11-90@yandex.by>
2025-06-28 19:39:19 +0200

    feature: enable custom service worker with caching strategies

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -6,17 +6,50 @@
  * quasar.config file > pwa > workboxMode is set to "InjectManifest"
  */
 
-import { clientsClaim } from 'workbox-core'
-import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching'
-import { registerRoute, NavigationRoute } from 'workbox-routing'
-
-self.skipWaiting()
-clientsClaim()
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import {
+  StaleWhileRevalidate,
+  CacheFirst,
+  NetworkFirst,
+} from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 // Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST);
 
-cleanupOutdatedCaches()
+//caching strategies
+registerRoute(
+  ({ url }) => url.host.startsWith('fonts.g'),
+  new CacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 30,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/posts'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  ({ url }) => url.href.startsWith('http'),
+  new StaleWhileRevalidate()
+);
+
+/*
+self.skipWaiting();
+clientsClaim();
+
+cleanupOutdatedCaches();
 
 // Non-SSR fallbacks to index.html
 // Production SSR fallbacks to offline.html (except for dev)
@@ -24,7 +57,13 @@ if (process.env.MODE !== 'ssr' || process.env.PROD) {
   registerRoute(
     new NavigationRoute(
       createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
-      { denylist: [new RegExp(process.env.PWA_SERVICE_WORKER_REGEX), /workbox-(.)*\.js$/] }
+      {
+        denylist: [
+          new RegExp(process.env.PWA_SERVICE_WORKER_REGEX),
+          /workbox-(.)*\.js$/,
+        ],
+      }
     )
-  )
+  );
 }
+*/
